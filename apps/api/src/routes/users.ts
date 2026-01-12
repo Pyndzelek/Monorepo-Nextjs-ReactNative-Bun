d2501725fa600725a17cac9808142a2ed87f89e2
kamil <kamilmaslanka212@gmail.com>
2026-01-12 16:11:39 +0100

    better-auth shemas creation

--- apps/api/src/routes/users.ts
+++ apps/api/src/routes/users.ts
@@ -1,16 +1,9 @@
 import { Hono } from "hono";
-import { users } from "@repo/db/schemas";
+
 import { db } from "@repo/db/client";
 import { createUserSchema } from "@repo/db/validation";
 
 //This function runs as a middleware between each request and response
 import { zValidator } from "@hono/zod-validator";
 
-export const usersRoute = new Hono()
-  .get("/", async (c) => {
-    const result = await db.select().from(users);
-    return c.json(result);
-  })
-  .post("/", zValidator("json", createUserSchema), async (c) => {
-    return c.json({ message: "User created" });
-  });
+export const usersRoute = new Hono();
